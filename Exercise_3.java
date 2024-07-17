e0fc7fb7f635f0d8a53723ee9883a331bcff93a9
PKULK2 <48229592+PKULK2@users.noreply.github.com>
2024-07-17 10:23:09 -0400

    Completed Exercise_3.java

--- Exercise_3.java
+++ Exercise_3.java
@@ -1,3 +1,7 @@
+// Time Complexity : O(1)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 import java.io.*; 
   
 // Java program to implement 
@@ -18,6 +22,8 @@ public class LinkedList {
         Node(int d) 
         { 
             //Write your code here 
+          data = d;
+          next = null;
         } 
     } 
   
@@ -25,26 +31,41 @@ public class LinkedList {
     public static LinkedList insert(LinkedList list, int data) 
     { 
         // Create a new node with given data 
-   
+       Node newNode = new Node(data);
         // If the Linked List is empty, 
         // then make the new node as head 
-        
+        if (list.head == null){
+          list.head == newNode;
+        }
+        else {
             // Else traverse till the last node 
             // and insert the new_node there 
 
             // Insert the new_node at last node 
         // Return the list by head 
-        
+        Node tail = list.head;
+            while (tail.next != null) {
+                tail = tail.next;
+            }
+            tail.next = newNode;
+
+        }
+      return list;
     } 
   
     // Method to print the LinkedList. 
     public static void printList(LinkedList list) 
     {  
         // Traverse through the LinkedList 
-   
+        Node temp = list.head;
+        while (temp != null) {
             // Print the data at current node 
        
+            System.out.print(temp.data + " -> ");
             // Go to next node 
+            temp = temp.next;
+        }
+        System.out.print( "NULL");
     } 
    
     // Driver code 
